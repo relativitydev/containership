88a04d115a65268213c2caec07bf894ab18811a4
Chris Lenard <christopher.lenard@relativity.com>
2021-06-02 12:50:23 +0000

    Typos

--- version/version.go
+++ version/version.go
@@ -1,7 +1,7 @@
 package version
 
 var (
-	// Version defines the current version of keda
+	// Version defines the current version of containership
 	Version = "0.0.1"
 	// GitCommit stores the current commit hash
 	GitCommit string
